f7ef3a3e0a0f6688c37ee2a2b9b1be12e0c133ea
mascott82 <m.a.scott@live.com>
2023-12-17 22:10:22 -0600

    Initializing - Adjusted the default value of the timestamp.

--- db/seeds/03_favourites.sql
+++ db/seeds/03_favourites.sql
@@ -1,11 +1,11 @@
 -- favourites table seeds here (Example)
-INSERT INTO favourites (user_id, item_listing_id) VALUES (1, 1);
-INSERT INTO favourites (user_id, item_listing_id) VALUES (2, 2);
-INSERT INTO favourites (user_id, item_listing_id) VALUES (1, 3);
-INSERT INTO favourites (user_id, item_listing_id) VALUES (3, 4);
-INSERT INTO favourites (user_id, item_listing_id) VALUES (2, 5);
-INSERT INTO favourites (user_id, item_listing_id) VALUES (4, 6);
-INSERT INTO favourites (user_id, item_listing_id) VALUES (3, 7);
-INSERT INTO favourites (user_id, item_listing_id) VALUES (1, 8);
-INSERT INTO favourites (user_id, item_listing_id) VALUES (4, 9);
-INSERT INTO favourites (user_id, item_listing_id) VALUES (2, 10);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (1, 1, CURRENT_TIMESTAMP);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (2, 2, CURRENT_TIMESTAMP);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (1, 3, CURRENT_TIMESTAMP);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (3, 4, CURRENT_TIMESTAMP);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (2, 5, CURRENT_TIMESTAMP);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (4, 6, CURRENT_TIMESTAMP);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (3, 7, CURRENT_TIMESTAMP);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (1, 8, CURRENT_TIMESTAMP);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (4, 9, CURRENT_TIMESTAMP);
+INSERT INTO favourites (user_id, item_listing_id, created_at) VALUES (2, 10, CURRENT_TIMESTAMP);
