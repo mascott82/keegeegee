f468542ffb2f8a8ea6f1f9503bc3635a52a75460
mascott82 <m.a.scott@live.com>
2023-12-19 13:39:52 -0600

    Featuring - Added a column for user phone number.

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -6,6 +6,6 @@ CREATE TABLE users (
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  full_name VARCHAR(255),
+  phone_number VARCHAR(255),
   created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
