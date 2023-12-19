a2807dcb302a7eaee769a6858c137e09bf05d8e3
mascott82 <m.a.scott@live.com>
2023-12-19 15:00:34 -0600

    Featuring - Added header to the message page.

--- routes/message.js
+++ routes/message.js
@@ -12,9 +12,10 @@ const msgs = require('../db/queries/messages');
 
 router.get('/list', (req, res) => {
   const toUserId = req.session.userId ? req.session.userId : 1;
+  const toUserName = req.session.username ? req.session.username : '';
   msgs.getMessagesByUser(toUserId)
     .then((result) => {
-      res.render("messages", { result });
+      res.render("messages", { result: result, userId: toUserId, username: toUserName });
     })
     .catch(error => {
       console.error("Error retrieving the message. ", error);
