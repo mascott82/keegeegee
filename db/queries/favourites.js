7b6d76d217582e67bcd767ad9531357dd357df64
yoonjookim <yjblossom@gmail.com>
2023-12-22 09:58:25 -0500

    show single at fav, delete single at fav

--- db/queries/favourites.js
+++ db/queries/favourites.js
@@ -3,7 +3,27 @@ const ROWLIMIT = 10;
 const getFavourites = async function(_userid, _rowLimit = ROWLIMIT) {
   // status is currently set as hard-coded = "active", TODO: add status to items table or status table.
   let _qryString =
-  `With fav as (SELECT * from favourites WHERE user_id = ${_userid})
+  `With fav as (
+    SELECT k.*
+    FROM
+    (SELECT
+      *,
+      row_number()
+    OVER
+      (
+        PARTITION BY
+        user_id,
+        item_listing_id
+        ORDER BY
+        id
+      ) as created_order
+    FROM
+    favourites
+    WHERE
+    user_id = ${_userid}
+    ) as k
+    WHERE k.created_order =1
+  )
   SELECT
   a.id,
   a.user_id,
@@ -25,7 +45,8 @@ const getFavourites = async function(_userid, _rowLimit = ROWLIMIT) {
   ON
   a.user_id = c.id
   ORDER BY a.id desc
-  LIMIT ${_rowLimit}`;
+  LIMIT ${_rowLimit}
+  `;
 
   try {
     const data = await db.query(_qryString);
@@ -51,7 +72,12 @@ const addFavourite = (favourite) => {
 
 const deleteFavourite = (favId) =>{
   // with the given favId, delete corresponding row from favourites table
-  let _deleteQry = `DELETE FROM favourites WHERE id = ${favId};`;  // complete query by using string interpolation
+  let _deleteQry = `DELETE FROM favourites
+  WHERE
+  user_id = (select distinct user_id from favourites where id = ${favId})
+  AND
+  item_listing_id = (select distinct item_listing_id from favourites where id = ${favId})
+  ;`;  // complete query by using string interpolation
   db.query(_deleteQry)  // db is middle guy and handle with the given query
     .then((data) => {  // db.query is async, need "then" for success case
       return data.rows;
